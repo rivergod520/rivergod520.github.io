f2f2f265fc4f09553ad148047299e1e82e5f56cd
rivergod520 <76861853+rivergod520@users.noreply.github.com>
2022-08-06 16:14:04 +0800

    Update index.php

--- wallpapers/index.php
+++ wallpapers/index.php
@@ -1,15 +1,13 @@
-7 lines (14 sloc)  437 Bytes
-
 <!DOCTYPE html>
 <html>
 <body>
 
 <?php 
-$myLinks = array("https://takagen99.github.io/wallpapers/01.png", 
-    "https://takagen99.github.io/wallpapers/02.png",
-    "https://takagen99.github.io/wallpapers/03.png", 
-    "https://takagen99.github.io/wallpapers/04.png", 
-    "https://takagen99.github.io/wallpapers/05.png");
+$myLinks = array("rivergod520.github.io/wallpapers/01.png", 
+    "https://rivergod520.github.io/wallpapers/02.png",
+    "https://rivergod520.github.io/wallpapers/03.png", 
+    "https://rivergod520.github.io/wallpapers/04.png", 
+    "https://rivergod520.github.io/wallpapers/05.png");
 
 $randomRedirection = $myLinks[array_rand($myLinks)]; 
 header("Location: $randomRedirection"); 
