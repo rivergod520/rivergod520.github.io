6606e2d65600f2473df55cb4602fc91f99cf7943
rivergod520 <76861853+rivergod520@users.noreply.github.com>
2022-08-06 16:21:43 +0800

    Update index.php

--- wallpapers/index.php
+++ wallpapers/index.php
@@ -6,8 +6,10 @@
 $myLinks = array("rivergod520.github.io/wallpapers/01.png", 
     "https://rivergod520.github.io/wallpapers/02.png",
     "https://rivergod520.github.io/wallpapers/03.png", 
-    "https://rivergod520.github.io/wallpapers/04.png", 
-    "https://rivergod520.github.io/wallpapers/05.png");
+    "https://rivergod520.github.io/wallpapers/04.png",
+    "https://rivergod520.github.io/wallpapers/07.png", 
+    "https://rivergod520.github.io/wallpapers/08.png", 
+    "https://rivergod520.github.io/wallpapers/09.png");
 
 $randomRedirection = $myLinks[array_rand($myLinks)]; 
 header("Location: $randomRedirection"); 
